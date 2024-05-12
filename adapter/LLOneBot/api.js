44f07f910eb4dd0a97b60bd5732a24d479c201be
XZhouQD <X.Zhou.QD@hotmail.com>
2024-05-12 15:52:11 +0800

    try fix forward msg

--- adapter/LLOneBot/api.js
+++ adapter/LLOneBot/api.js
@@ -693,7 +693,7 @@ let api = {
     if (node) {
       // const id = await this.SendApi(uin, 'send_forward_msg', { messages: message.map(i => i.data) })
       // res = await this.SendApi(uin, 'send_private_msg', { user_id, message: { type: 'forward', data: { id } } })
-      res = await this.send_private_forward_msg(uin, user_id, message)
+      res = await this.send_private_forward_msg(uin, user_id, message.map(i => i.data))
     } else {
       const params = { user_id, message }
       res = await this.SendApi(uin, 'send_private_msg', params)
@@ -728,7 +728,7 @@ let api = {
     if (node) {
       // const id = await this.SendApi(uin, 'send_forward_msg', { messages: message.map(i => i.data) })
       // res = await this.SendApi(uin, 'send_group_msg', { group_id, message: { type: 'forward', data: { id } } })
-      res = await this.send_group_forward_msg(uin, group_id, message)
+      res = await this.send_group_forward_msg(uin, group_id, message.map(i => i.data))
     } else {
       const params = { group_id, message }
       res = await this.SendApi(uin, 'send_group_msg', params)
