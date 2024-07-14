abe55fc816dddc48e7bdb1b6581d79bfb2b7198f
kongkongwa <136422328+kongkongwa@users.noreply.github.com>
2024-07-15 03:07:17 +0800

    修复官bot发图

--- adapter/Bot/bot.js
+++ adapter/Bot/bot.js
@@ -7,6 +7,7 @@ import crypto from 'crypto'
 import common from '../../lib/common/common.js'
 import Cfg from '../../lib/config/config.js'
 import { fileTypeFromBuffer } from 'file-type'
+import { Stream } from 'stream'
 
 /**
 * 传入文件，返回Buffer
@@ -23,7 +24,7 @@ Bot.Buffer = async function (file, data) {
   if (Buffer.isBuffer(file) || file instanceof Uint8Array) {
     if (data?.buffer) return file
     return file
-  } else if (file instanceof fs.ReadStream) {
+  } else if (file instanceof fs.ReadStream || file instanceof Stream.PassThrough) {
     return await Bot.Stream(file)
   } else if (fs.existsSync(file.replace(/^file:\/\//, ''))) {
     if (data?.file) return file
@@ -62,7 +63,7 @@ Bot.Base64 = async function (file, data) {
   if (Buffer.isBuffer(file) || file instanceof Uint8Array) {
     if (data?.buffer) return file
     return file.toString('base64')
-  } else if (file instanceof fs.ReadStream) {
+  } else if (file instanceof fs.ReadStream || file instanceof Stream.PassThrough) {
     return await Bot.Stream(file, { base: true })
   } else if (fs.existsSync(file.replace(/^file:\/\//, ''))) {
     if (data?.file) return file
@@ -126,6 +127,7 @@ Bot.uploadQQ = async function (file, uin = Bot.uin) {
   const url = `https://gchat.qpic.cn/gchatpic_new/0/0-0-${md5}/0?term=2`
   return { width, height, url, md5 }
 }
+
 
 /**
 * 传入文件，转为服务器公网url
@@ -212,7 +214,7 @@ Bot.FileToPath = async function (file, _path) {
   if (Buffer.isBuffer(file) || file instanceof Uint8Array) {
     fs.writeFileSync(_path, file)
     return _path
-  } else if (file instanceof fs.ReadStream) {
+  } else if (file instanceof fs.ReadStream || file instanceof Stream.PassThrough) {
     const buffer = await Bot.Stream(file)
     fs.writeFileSync(_path, buffer)
     return _path
@@ -270,7 +272,7 @@ Bot.toType = function (i) {
   } else if (i?.type === 'Buffer' || i instanceof Uint8Array || Buffer.isBuffer(i?.data || i)) {
     type = 'buffer'
     file = i?.data || i
-  } else if (i instanceof fs.ReadStream || i?.path) {
+  } else if (i instanceof fs.ReadStream || i?.path || i instanceof Stream.PassThrough ) {
     // 检查是否是ReadStream类型
     if (fs.existsSync(i.path)) {
       file = `file://${i.path}`
@@ -337,7 +339,7 @@ Bot.FormatFile = async function (file) {
       if (Buffer.isBuffer(file) || file instanceof Uint8Array) return file
 
       /** 流 */
-      if (file instanceof fs.ReadStream) return await Bot.Stream(file, { base: true })
+      if (file instanceof fs.ReadStream || file instanceof Stream.PassThrough) return await Bot.Stream(file, { base: true })
 
       /** i.file */
       if (file.file) return str(file.file)
@@ -418,7 +420,16 @@ Bot.Button = function (list, line = 3) {
     if (Array.isArray(i)) {
       button.push(...Bot.Button(i, 10))
     } else {
-      arr.push({
+      if (typeof i.permission === 'string') {
+        if (i.permission === 'xxx') {
+          i.list = []
+        } else {
+          const openid = i.permission.split('-')
+          i.list = [openid[1] || openid[0]]
+        }
+        i.permission = false
+      }
+      let Button = {
         id: String(id),
         render_data: {
           label: i.text || i.label || i.link,
@@ -429,15 +440,22 @@ Bot.Button = function (list, line = 3) {
           type: i.type || (i.link ? 0 : 2),
           reply: i.reply || false,
           permission: i.permission || {
-            type: (i.admin && 1) || (i.list && 0) || (i.role && 3) || 2,
+            type: (i.admin && 1) || (i.list && '0') || (i.role && 3) || 2,
             specify_user_ids: i.list || [],
             specify_role_ids: i.role || []
           },
-          data: i.data || i.callback || i.link || i.text || i.label,
+          data: i.data || i.input || i.callback || i.link || i.text || i.label,
           enter: i.send || i.enter || 'callback' in i || false,
           unsupport_tips: i.tips || 'err'
         }
-      })
+      }
+      if (i.QQBot) {
+        if (i.QQBot.render_data)
+          Object.assign(Button.render_data, i.QQBot.render_data)
+        if (i.QQBot.action)
+          Object.assign(Button.action, i.QQBot.action)
+      }
+      arr.push(Button)
       if (index % line == 0 || index == list.length) {
         button.push({
           type: 'button',
